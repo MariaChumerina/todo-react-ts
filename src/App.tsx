a7ed5d63aa129d632b737309aec3e9a71f4592c3
MariaChumerina <masha.chumerina@gmail.com>
2021-01-20 17:51:47 +0500

    add todolist with part of logic add interfaces.ts add styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 import './App.css';
 import { Navbar } from "./components/Navbar";
 import { TodoForm } from "./components/TodoForm";
+import { TodoList } from "./components/TodoList";
+import { Todo } from "./interfaces";
 
 const App: React.FC = () => {
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState<Todo[]>([]);
 
     const addHandler = (title: string) => {
-        console.log('add new todo', title);
+        const newTodo: Todo = {
+            title,
+            id: Date.now(),
+            completed: false,
+        }
+        setTodos(prev => [newTodo, ...prev]);
     }
 
     return (
@@ -15,6 +22,7 @@ const App: React.FC = () => {
         <Navbar />
         <div className="container">
             <TodoForm onAdd={addHandler}/>
+            <TodoList todos={todos} />
         </div>
       </>
     );
